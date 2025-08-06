be5f7705c474442628f13db358ec43dd72052c2f
ahyeonbaek <4641090@naver.com>
2025-08-06 23:06:28 +0900

    base 경로 수정

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/myportfolio/" : "/",
+  base: mode === "production" ? "/PortFolio/" : "/",
   plugins: [react()],
   resolve: {
     alias: [
