f4b2a39e79786ffd2452abc147fea85a6539b8d0
ahyeonbaek <4641090@naver.com>
2025-08-07 03:17:43 +0900

    주석해제

--- src/Route.tsx
+++ src/Route.tsx
@@ -14,10 +14,10 @@ const router = createBrowserRouter(
         },
       ],
     },
-  ]
-  // {
-  //   basename: "/PortFolio",
-  // }
+  ],
+  {
+    basename: "/PortFolio",
+  }
 );
 
 export default router;
